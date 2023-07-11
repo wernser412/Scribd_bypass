6bb4a6eda3054b0974d591efcbb3b525422b887f
wernser412 <53667212+wernser412@users.noreply.github.com>
2023-07-11 15:22:21 -0500

    Add files via upload

---
+++ Scribd bypass.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name               Scribd bypass
+// @description        Script disables blur on text
+// @author             wernser412
+// @version            2023/07/11
+// @license            MIT
+// @namespace          https://github.com/wernser412
+// @match              *://*.scribd.com/*
+// @icon               https://s-f.scribdassets.com/favicon.ico
+// @require            https://code.jquery.com/jquery-3.7.0.min.js
+// ==/UserScript==
+/* eslint-env jquery */
+$(document).ready(function(){
+    'use strict';
+    if (window.location.href.match(/(document|presentation)\/(\d+)\//) == null) return;
+    //Remove blocks between pages
+    $('div.between_page_module').remove();
+
+    //Remove banners on pages
+    $('div.auto__doc_page_webpack_doc_page_blur_promo').remove();
+
+    // Remove blur
+    $('div.newpage div.text_layer').css('text-shadow', 'black 0px 0px 0px');
+
+    // Remove unselectable attribute
+    $('[unselectable]').removeAttr('unselectable');
+
+    // Remove blurred_page class
+    $('.blurred_page').removeClass('blurred_page');
+
+    //Remove banners promo
+    $('.promo_div').remove();
+
+})();
